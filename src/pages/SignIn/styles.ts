7aec252699ef9e0688a5e471070338c25bc532d6
Zaqueu Santos <zaqueulima1@gmail.com>
2020-08-27 07:59:06 -0300

    style: remove semi from sign in styles

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,13 +1,13 @@
-import styled from 'styled-components/native';
+import styled from 'styled-components/native'
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   align-items: center;
-`;
+`
 
 export const Logo = styled.Image`
   margin-top: 100px;
-`;
+`
 
 export const Title = styled.Text`
   font-size: 20px;
@@ -15,13 +15,13 @@ export const Title = styled.Text`
   color: #47455a;
   margin-top: 50px;
   margin-bottom: 24px;
-`;
+`
 
 export const Form = styled.View`
   width: 100%;
   padding: 0 36px;
   align-items: center;
-`;
+`
 
 export const TextInput = styled.TextInput`
   width: 100%;
@@ -36,7 +36,7 @@ export const TextInput = styled.TextInput`
   margin-bottom: 10px;
   border-width: 0.5px;
   border-color: #201027;
-`;
+`
 
 export const Button = styled.TouchableOpacity`
   background-color: #3a2875;
@@ -47,23 +47,23 @@ export const Button = styled.TouchableOpacity`
   margin-bottom: 9px;
   justify-content: center;
   align-items: center;
-`;
+`
 
 export const ButtonText = styled.Text`
   text-transform: uppercase;
   color: #ffffff;
   font-size: 16px;
   font-family: 'Montserrat-Bold';
-`;
+`
 
 export const SmallText = styled.Text`
   color: #1b1238;
   font-size: 16px;
   font-family: 'Montserrat-Regular';
-`;
+`
 
 export const Footer = styled.View`
   flex: 1;
   flex-direction: column-reverse;
   padding: 23px 0;
-`;
+`
