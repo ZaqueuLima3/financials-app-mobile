6ab5d04431b475e36003a9c4024198f3e14f7d57
Zaqueu Santos <zaqueulima1@gmail.com>
2020-08-26 21:36:06 -0300

    feat: start sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import {SafeAreaView, Text, StatusBar} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
 declare const global: {HermesInternal: null | {}};
 
+import Routes from './routes';
+
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <StatusBar barStyle="dark-content" />
-        <Text>Hello everyone</Text>
-      </SafeAreaView>
+      <Routes />
     </NavigationContainer>
   );
 };
---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import SignIn from '../pages/SignIn';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => {
+  return (
+    <Auth.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: '#FFF',
+        },
+      }}>
+      <Auth.Screen name="SignIn" component={SignIn} />
+    </Auth.Navigator>
+  );
+};
+
+export default AuthRoutes;
