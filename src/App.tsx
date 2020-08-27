7bcc3dfce7e96a5b663035da25571a02b8e61de1
Zaqueu Santos <zaqueulima1@gmail.com>
2020-08-26 21:23:03 -0300

    chore: add react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import {SafeAreaView, Text, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 declare const global: {HermesInternal: null | {}};
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <SafeAreaView>
         <StatusBar barStyle="dark-content" />
         <Text>Hello everyone</Text>
       </SafeAreaView>
-    </>
+    </NavigationContainer>
   );
 };
 
