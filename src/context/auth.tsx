0e66d4ea5fcccbb8fc76c0f54abcdaf317a36321
Zaqueu Santos <zaqueulima1@gmail.com>
2020-12-08 14:25:08 -0300

    feat: add signin and signup

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,4 +1,18 @@
-import React, {createContext, useState} from 'react'
+import React, {createContext, useState, useCallback, useEffect} from 'react'
+import AsyncStorage from '@react-native-community/async-storage'
+import api from '../service/api'
+
+interface User {
+  id: string
+  name: string
+  email: string
+  avatar_url: string
+}
+
+interface AuthState {
+  token: string
+  user: User
+}
 
 interface SignInCredentials {
   email: string
@@ -6,28 +20,58 @@ interface SignInCredentials {
 }
 
 export interface AuthContextData {
-  logged: boolean
   signIn(credentials: SignInCredentials): void
   signOut(): void
+  user: User
+  loading: boolean
 }
 
 export const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
 export const AuthProvider: React.FC = ({children}) => {
-  const [logged, setLogged] = useState(true)
+  const [data, setData] = useState<AuthState>({} as AuthState)
+  const [loading, setLoading] = useState(true)
 
-  function signIn(credentials: SignInCredentials): void {
-    if (credentials.email && credentials.password) {
-      setLogged(true)
+  useEffect(() => {
+    async function loadData(): Promise<void> {
+      const [token, user] = await AsyncStorage.multiGet([
+        '@youplan:token',
+        '@youplan:user',
+      ])
+
+      if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`
+        setData({token: token[1], user: JSON.parse(user[1])})
+      }
+      setLoading(false)
     }
-  }
 
-  function signOut(): void {
-    setLogged(false)
-  }
+    loadData()
+  }, [])
+
+  const signIn = useCallback(async ({email, password}: SignInCredentials) => {
+    const response = await api.post('sessions', {email, password})
+
+    const {token, user} = response.data
+
+    api.defaults.headers.authorization = `Bearer ${token}`
+
+    await AsyncStorage.multiSet([
+      ['@youplan:token', token],
+      ['@youplan:user', JSON.stringify(user)],
+    ])
+
+    setData({token, user})
+  }, [])
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(['@youplan:token', '@youplan:user'])
+
+    setData({} as AuthState)
+  }, [])
 
   return (
-    <AuthContext.Provider value={{logged, signIn, signOut}}>
+    <AuthContext.Provider value={{user: data.user, signIn, signOut, loading}}>
       {children}
     </AuthContext.Provider>
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
+import {ActivityIndicator, View} from 'react-native'
 import {useAuth} from '../hooks/auth'
 
 import AuthRoutes from './auth.routes'
 import AppRoutes from './app.routes'
 
 const Routes: React.FC = () => {
-  const {logged} = useAuth()
+  const {user, loading} = useAuth()
 
-  return logged ? <AppRoutes /> : <AuthRoutes />
+  if (loading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" color="#aaa" />
+      </View>
+    )
+  }
+
+  return user ? <AppRoutes /> : <AuthRoutes />
 }
 
 export default Routes
